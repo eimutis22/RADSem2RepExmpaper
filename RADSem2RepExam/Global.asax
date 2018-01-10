cebe378c6a2cb6b3a757d2dcc9ea4301e6a72276
Eimutis <eimutis.genys@gmail.com>
2018-01-10 10:57:29 +0000

    Create Project

---
+++ RADSem2RepExam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RADSem2RepExam.MvcApplication" Language="C#" %>
---
+++ RADSem2RepExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RADSem2RepExam.Startup))]
+namespace RADSem2RepExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
