236c6e9e5e15dd0792d2ee11be111b4bf7d7d176
Eimutis <eimutis.genys@gmail.com>
2018-01-10 11:17:28 +0000

    StudentDbContext added

---
+++ RADSem2RepExam/Models/StudentModels/StudentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RADSem2RepExam.Models.StudentModels
+{
+    public class StudentDbContext: DbContext
+    {
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public StudentDbContext() : base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
